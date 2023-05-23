945730ad54dc6403a430f9647dae81d93b659446
lipnganga <philipmurage98@gmail.com>
2023-05-23 20:05:17 +0300

    solutions to task 4-7

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -3,7 +3,7 @@
 """
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
@@ -11,7 +11,7 @@ class Config(object):
     """_summary_
 
     Returns:
-            _type_: _description_
+                    _type_: _description_
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -25,12 +25,37 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """returns a user dictionary or None if the ID cannot be found
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """_summary_
+    """
+    user = get_user()
+    g.user = user
+
+
 @babel.localeselector
 def get_locale():
     """_summary_
 
     Returns:
-            _type_: _description_
+                    _type_: _description_
     """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
@@ -46,7 +71,7 @@ def get_locale():
 def index():
     """_summary_
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
